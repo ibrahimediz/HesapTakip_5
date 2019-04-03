a857c4ac8883009737c9ef12e8675f6c9d1d0fcc
ediz <ediz@ediz.com>
2019-04-03 20:09:53 +0300

    verieklendi

--- Ana.py
+++ Ana.py
@@ -2,12 +2,41 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit,QMainWindow
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import uic
-
+from ilkDB import ilkDB
 class Ana(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__()
+        self.veriTaban = ilkDB()
+        self.secilenAy = ""
+        self.secilenKalem = ""
         self.win = uic.loadUi(r"D:\İbrahim EDİZ\HesapTakip_5\Ana.ui")
+
+        self.win.cmbAy.currentIndexChanged.connect(self.cmbAyClick)
+        self.win.cmbKalem.currentIndexChanged.connect(self.cmbKalemClick)
+
+        self.win.btKaydet.clicked.connect(self.btKaydeyClick)
+
+        for a,b in self.veriTaban.sozlukGetir(1):
+            self.win.cmbKalem.addItem(b,a)
+        for a,b in self.veriTaban.sozlukGetir(2):
+            self.win.cmbAy.addItem(b,a)
+        
+    
+    def btKaydeyClick(self):
+        tutar =  self.win.txtTutar.text()
+        self.veriTaban.VeriEkle(self.secilenKalem,self.secilenAy,tutar)
+
+
+    def cmbAyClick(self,i):
+        self.secilenAy = self.win.cmbAy.itemData(i)
+    def cmbKalemClick(self,i):
+        self.secilenKalem = self.win.cmbKalem.itemData(i)
+
+
+        
         self.win.show()
+
+    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- ilkDB.py
+++ ilkDB.py
@@ -1,18 +1,35 @@
 from DB import VeriTabani
+import sqlite3 as sql
 class ilkDB():
     def __init__(self):
         self.adres = r"D:\İbrahim EDİZ\HesapTakip_5\IEDB.db"
 
         
 
-    def VeriEkle(self,text1,text2):
+    def VeriEkle(self,kalem,ay,tutar):
         import sqlite3
         db = sqlite3.connect(self.adres)
         cursor = db.cursor()
-        cursor.execute("""INSERT INTO ARAYUZ_DENEME 
-        (TEXT1,TEXT2) VALUES 
-        ('{}','{}')""".format(text1,text2))
+        cursor.execute("""
+                INSERT INTO HSP_BILGI (HSP_KALEM,HSP_TUTAR,HSP_AY)
+                VALUES (
+                {},
+                {},
+                {}
+                )
+                """.format(kalem,tutar,ay)
+         )
         db.commit()
+
+    
+
+    def sozlukGetir(self,tabloid):
+        db = sql.connect(self.adres)
+        cursor = db.cursor()
+        cursor.execute("""
+        SELECT sozluk_id,SOZLUK_ADI FROM HSP_SOZLUK WHERE TABLO_ID = {}""".format(tabloid)
+        )
+        return cursor.fetchall()
 
 
         # print(text1,text2)
